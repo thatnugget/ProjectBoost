81f2de1b3a09b72ec9ed9b729f3b33ff4cc546f3
Patrick Nugent <thatnugget@Patricks-MacBook-Pro.local>
2018-01-20 23:36:47 -0500

    rocket script

---
+++ Assets/rocket.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rocket : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		ProcessInput ();
+	}
+
+
+	private void ProcessInput(){
+		
+		if (Input.GetKey(KeyCode.Space)){
+			Debug.Log ("thrusting");
+		}
+
+		if (Input.GetKey(KeyCode.A)){
+			Debug.Log ("rotate left");
+		}
+
+		else if (Input.GetKey(KeyCode.D)){
+			Debug.Log ("rotate right");
+		}
+
+	
+	}
+}
